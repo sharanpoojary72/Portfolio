ba98571b48a22d318bc8da76b3e81492be5d0a69
Sharan Poojary <sharanpoojary72@gmail.com>
2024-08-28 12:47:35 +0530

    about section update

--- darkmode.js
+++ darkmode.js
@@ -3,18 +3,22 @@ let lightmode = localStorage.getItem('lightmode');
 const themeSwitch = document.getElementById('theme-switch');
 
 let videoprop = document.querySelector("#background-video");
+let aboutimg = document.querySelector('.aboutimg');
 
 const enableLightMode =() => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode','active');
     videoprop.style.filter =" invert(0%) var(--video-color-hue) saturate(5) opacity(0.4)";
     document.body.style.transition = "all 0.4s ease-in-out";
+    aboutimg.style.backgroundImage = "url(./images/aboutblack.png)";
 }
 const disableLightMode =() => {
     document.body.classList.remove('lightmode');
     localStorage.setItem('lightmode',null);
     videoprop.style.filter =" invert(100%) var(--video-color-hue) saturate(5) opacity(0.4)";
     document.body.style.transition = "all 0.4s ease-in-out";
+    aboutimg.style.backgroundImage = "url(./images/aboutwhite.png)";
+
 }
 
 if(lightmode === 'active') enableLightMode();
@@ -25,9 +29,10 @@ themeSwitch.addEventListener('click',()=>{
 })
 
 
-
+//Checks and applies the mode to video
 if(lightmode === 'active'){
     videoprop.style.filter =" invert(0%) var(--video-color-hue) saturate(5) opacity(0.4)";
+    
 }
 else{
     videoprop.style.filter =" invert(100%) var(--video-color-hue) saturate(5) opacity(0.4)";
